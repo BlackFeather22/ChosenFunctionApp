7e891ecd957188d6f81c0607a85cb2f81ee8e484
BlackFeather22 <Mojtaba_n.m.m@windowslive.com>
2023-03-26 11:11:21 +0300

    Added a function that connects to the Database and retireve its entries and returns it to the calling application

--- ChosenFunctionApp/FirstFunction.cs
+++ ChosenFunctionApp/FirstFunction.cs
@@ -8,6 +8,9 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Azure.WebJobs.Host;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
+using System.Net;
+using System.Net.Http;
+using System.Collections.Generic;
 
 namespace ChosenFunctionApp;
 
@@ -30,4 +33,45 @@ public static class FirstFunction
             : new BadRequestObjectResult("Please pass a name on the query string or in the request body");
         
     }
+    
 }
+
+public static class DatabaseFunction
+{
+    [FunctionName("DatabaseFunction")]
+    public static async Task<HttpResponseMessage> RunAsync(
+        [HttpTrigger(AuthorizationLevel.Function, "get", Route = null)] HttpRequestMessage req)
+    {
+        // Replace with the URL of your API and the name of the table
+        string tableName = "MyTable";
+        string apiUrl = $"https://xchosen.database.windows.net/entries?table={tableName}&top=5";
+
+        // Make the HTTP GET request to the API
+        using (var client = new HttpClient())
+        {
+            var response = await client.GetAsync(apiUrl);
+            if (response.IsSuccessStatusCode)
+            {
+                // Parse the response and extract the data
+                var content = await response.Content.ReadAsStringAsync();
+                var data = JsonConvert.DeserializeObject<List<Entry>>(content);
+
+                // Return the data as a JSON response
+                return req.CreateResponse(HttpStatusCode.OK, data);
+            }
+            else
+            {
+                // Return an error response if the API call failed
+                return req.CreateErrorResponse(response.StatusCode, response.ReasonPhrase);
+            }
+        }
+    }
+
+    public class Entry
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+    
+}
+
